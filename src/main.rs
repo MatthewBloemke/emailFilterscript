371f8290035918af4905b76723066a3d2bb4a614
MatthewBloemke <mattbloemke@gmail.com>
2022-03-09 23:14:18 -0600

    adding function to eliminate unneeded for loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::{create_dir, read_dir, write, File};
 use std::io::{stdin, BufRead, BufReader};
+use std::collections::{HashMap};
+use std::iter::Iterator;
 
 fn main() {
     println!("What folder would you like to filter? (must be relative folder, i.e. './example')");
@@ -42,20 +44,42 @@ fn filter_email_folder(input_folder_path: String, output_folder_path: String) {
 }
 
 fn filter_email_file(input_folder_path: &str, output_folder_path: &str, file_name: String) {
-    let file = File::open(format!("{}/{}", input_folder_path, file_name)).unwrap();
-    let reader = BufReader::new(file);
+    let file = File::open(format!("{}/{}", input_folder_path, &file_name)).unwrap();
+    let reader = BufReader::new(&file);
+    let second_reader = BufReader::new(&file);
 
     let mut filtered_emails: Vec<String> = Vec::new();
 
+    let mut valid_domains: HashMap<String, String>  = HashMap::new();
+
+
     for line_or in reader.lines() {
+        let line_items: Vec<String> = line_or.unwrap().split(",").map(str::to_string).collect();
+
+        //println!("{}", line_items[0]);
+        let email: String = format!("{}", line_items[0]);
+        let company: String = format!("{}", line_items[1]);
+        let system: String = format!("{}", line_items[2]);
+
+
+        if !email.contains("@byetm") && !email.contains("@sovos") {
+            let correct_domain = format!("@{}", email.split("@").last().unwrap()); 
+            valid_domains.insert(company, correct_domain.to_string());
+        }
+        
+    }
+
+
+    for line_or in second_reader.lines() {
+        println!("second tab");
         let line: String = line_or.unwrap();
         let email: &str = line.split(",").next().unwrap();
-
+        println!("{}", line);
         if email.contains("@byetm") || email.contains("@sovos") {
             filtered_emails.push(line);
         }
     }
-
+    println!("{:?}", filtered_emails);
     write(
         format!("{}/{}{}", output_folder_path, "filtered_", file_name),
         filtered_emails.join("\r\n"),
